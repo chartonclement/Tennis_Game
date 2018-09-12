48fea220f10e56fe98a2b38d5bb5723093230e5b
chartonclement <43209309+chartonclement@users.noreply.github.com>
2018-09-12 19:52:15 +0300

    tennis game clement charton

--- src/Driver.java
+++ src/Driver.java
@@ -4,6 +4,34 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
+		play.winapoint(1);
+		play.getScore();
+		
+		play.winapoint(2);
+		play.getScore();
+		
+		play.winapoint(2);
+		play.getScore();
+		
+		play.winapoint(2);
+		play.getScore();
+		
+		play.winapoint(1);
+		play.getScore();
+		
+		play.winapoint(1);
+		play.getScore();
+		
+		play.winapoint(2);
+		play.getScore();
+		
+		play.winapoint(1);
+		play.getScore();
+		
+		play.winapoint(2);
+		play.getScore();
+		
+		play.winapoint(2);
 		play.getScore();
 	}
 
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -14,133 +14,121 @@ public class TennisGame {
 		System.out.println("I am constructor");
 		
 	}
-	public int random() {
-		int rand = 1 + (int)(Math.random() * (2));
-		return rand;
-	}
+
 	public void winapoint(int aplayer) {
 		if(aplayer==1) {
-			if (p1<3) {
+			System.out.println("   ");
+			System.out.println("point for player1");
+			if (p1<=3) {
 				p1= p1+1;
 			}
-			if (p1==3 && p2<=2) {
+			else if (p1==4 && p2==3) {
+				
 				p1=5;
 			}
-			if (p1==3 && p2==3) {
+			else if (p1==3 && p2<=2) {
+				
+				p1=5;
+			}
+			else if (p1==3 && p2==3) {
+				
 				p1=4;
 			}
-			if (p1==3 && p2==4) {
+			else {
 				p2=3;
 			}
-			if (p1==4 && p2==3) {
-				p1=5;
-			}
+			
 		}
 		if(aplayer==2) {
-			if (p2<3) {
+			System.out.println("   ");
+			System.out.println("point for player2");
+			if (p2<=3) {
 				p2=p2+1;
 			}
-			if (p2==3 && p1<=2) {
+			else if (p2==4 && p1==3) {
+					
 				p2=5;
 			}
-			if (p2==3 && p1==3) {
+			else if (p2==3 && p1<=2) {
+				
+				p2=5;
+			}
+			else if (p2==3 && p1==3) {
+				
 				p2=4;
 			}
-			if (p2==3 && p1==4) {
+			else {
+				
 				p1=3;
 			}
-			if (p2==4 && p1==3) {
-				p2=5;
-			}
+			
 		}
-		
 		
 	}
 
 	public String getScore() {
-		System.out.println("I am method for displaying score");
-		while (p1!=5 && p2!=5) {
-			
-			if (p1==0 && p2==0) {
-				System.out.println("0 - 0");
-			}
-			if (p1==1 && p2==0) {
-				System.out.println("15 - 0");
-			}
-			if (p1==2 && p2==0) {
-				System.out.println("30 - 0");
-			}
-			if (p1==3 && p2==0) {
-				System.out.println("40 - 0");
-			}
-			
-			if (p1==0 && p2==1) {
-				System.out.println("0 - 15");
-			}
-			if (p1==1 && p2==1) {
-				System.out.println("15 - 15");
-			}
-			if (p1==2 && p2==1) {
-				System.out.println("30 - 15");
-			}
-			if (p1==3 && p2==1) {
-				System.out.println("40 - 15");
-			}
-			if (p1==0 && p2==2) {
-				System.out.println("0 - 30");
-			}
-			if (p1==1 && p2==2) {
-				System.out.println("15 - 30");
-			}
-			if (p1==2 && p2==2) {
-				System.out.println("30 - 30");
-			}
-			if (p1==3 && p2==2) {
-				System.out.println("40 - 30");
-			}
-			if (p1==0 && p2==3) {
-				System.out.println("0 - 40");
-			}
-			if (p1==1 && p2==3) {
-				System.out.println("15 - 40");
-			}
-			if (p1==2 && p2==3) {
-				System.out.println("30 - 40");
-			}
-			if (p1==3 && p2==3) {
-				System.out.println("deuce");
-			}
-			if (p1==4 && p2==3) {
-				System.out.println("advantage player1");
-			}
-			if (p1==3 && p2==4) {
-				System.out.println("advantage player2");
-			}
-			winapoint(random());
-			
-			// Here is the format of the scores: "player1Score - player2Score"
-			// "0 - 0"
-			// "15 - 15"
-			// "30 - 30"
-			// "deuce"
-			// "15 - 0", "0 - 15"
-			// "30 - 0", "0 - 30"
-			// "40 - 0", "0 - 40"
-			// "30 - 15", "15 - 30"
-			// "40 - 15", "15 - 40"
-			// "advantage player1"
-			// "advantage player2"
-			// "game player1"
-			// "game player2"
-			}
-			if (p1==5) {
-				System.out.println("game player1");
-				return "end of the game";
-			}
-			if (p2==5) {
-				System.out.println("game player2");
-				return "end of the game";
-			}
+		
+		if (p1==0 && p2==0) {
+			System.out.println("0 - 0");
+		}
+		else if (p1==1 && p2==0) {
+			System.out.println("15 - 0");
+		}
+		else if (p1==2 && p2==0) {
+			System.out.println("30 - 0");
+		}
+		else if (p1==3 && p2==0) {
+			System.out.println("40 - 0");
+		}
+		
+		else if (p1==0 && p2==1) {
+			System.out.println("0 - 15");
+		}
+		else if (p1==1 && p2==1) {
+			System.out.println("15 - 15");
+		}
+		else if (p1==2 && p2==1) {
+			System.out.println("30 - 15");
+		}
+		else if (p1==3 && p2==1) {
+			System.out.println("40 - 15");
+		}
+		else if (p1==0 && p2==2) {
+			System.out.println("0 - 30");
+		}
+		else if (p1==1 && p2==2) {
+			System.out.println("15 - 30");
+		}
+		else if (p1==2 && p2==2) {
+			System.out.println("30 - 30");
+		}
+		else if (p1==3 && p2==2) {
+			System.out.println("40 - 30");
+		}
+		else if (p1==0 && p2==3) {
+			System.out.println("0 - 40");
+		}
+		else if (p1==1 && p2==3) {
+			System.out.println("15 - 40");
+		}
+		else if (p1==2 && p2==3) {
+			System.out.println("30 - 40");
+		}
+		else if (p1==3 && p2==3) {
+			System.out.println("deuce");
+		}
+		else if (p1==4 && p2==3) {
+			System.out.println("adventage player1");
+		}
+		else if (p1==3 && p2==4) {
+			System.out.println("adventage player2");
+		}
+		else if (p1==5) {
+			System.out.println("game to player1");
+		}
+		else if (p2==5) {
+			System.out.println("game to player2");
+		}
 	
 			// TO BE IMPLEMENTED
 			return "";
